76aff08394b27a8b66eba87fd388208d04c051db
tcrurav <tiburcio.cruz@gmail.com>
2024-04-30 16:48:29 +0100

    FIX other page

--- src/pages/other/Other.jsx
+++ src/pages/other/Other.jsx
@@ -1,8 +1,12 @@
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
 import "./Other.css";
 
-function Other(){
-  return(
+function Other() {
+  return (
     <>
+      <Header />
+      <Footer />
     </>
   )
 }
